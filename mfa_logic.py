736adad61d26fd5bd9b2522aa8c51f3b106fbdda
Andrea Ungerer <52411390+andirea19@users.noreply.github.com>
2026-03-03 12:31:58 +0100

    testfälle mit totp ausprobiert

--- mfa_logic.py
+++ mfa_logic.py
@@ -5,4 +5,25 @@ import time
 # Der ist noralerweise einzigartig, aber ich bin ja nur eine Person
 
 secret = pyotp.random_base32()
-print("Mein geheimer Schlüssel:", secret)    
+print("Mein geheimer Schlüssel:", secret)   
+
+totp = pyotp.TOTP('base32secret3232')
+totp.now() # => '492039'
+# Ich werd den aktuellen TOTP-Code generieren, damit ich ihn prüfen kann
+
+totp.verify('492039') # => True
+time.sleep(15)
+totp.verify('492039') # => False
+
+# ich schaue nach, ob der Code in den letzten 15 Sekunden gültig war
+totp.verify('492039', valid_window=1) # => True
+
+# das ergebnis wahr/falsch gebe ich aus
+if totp.verify('492039', valid_window=1):
+    print("Der Code ist gültig!")
+else:    print("Der Code ist ungültig!")    
+
+
+
+
+
